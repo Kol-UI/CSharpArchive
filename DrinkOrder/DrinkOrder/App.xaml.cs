d59ef870d916b21f1d00dd99e31c8d384356f143
Kol <62248045+Kol-UI@users.noreply.github.com>
2023-01-07 11:11:02 +0100

    Add files via upload

---
+++ DrinkOrder/DrinkOrder/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace DrinkOrder
+{
+	/// <summary>
+	/// Logique d'interaction pour App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
---
+++ DrinkOrder/DrinkOrder/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace DrinkOrder
+{
+	/// <summary>
+	/// Logique d'interaction pour MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void btOrderDrink_Click(object sender, RoutedEventArgs e)
+		{
+			int result;
+			if (string.IsNullOrWhiteSpace(tbDrink.Text))
+				MessageBox.Show("No drink ordered");
+			else if (string.IsNullOrWhiteSpace(tbSugars.Text))
+				MessageBox.Show($"You have ordered {tbDrink.Text}");
+			else if (int.TryParse(tbSugars.Text, out result))
+				MessageBox.Show($"You have ordered {tbDrink.Text} with {result} sugars");
+			else
+				MessageBox.Show("Enter a valid number for the sugars");
+		}
+	}
+}
